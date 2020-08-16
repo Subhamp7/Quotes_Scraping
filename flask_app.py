401bb2c43955cbd78323e04f027cc0ebb1827312
Subhamp7 <Subham306952@gmail.com>
2020-08-16 23:15:11 +0530

    changes the function parameters

--- flask_app.py
+++ flask_app.py
@@ -7,23 +7,22 @@ Created on Tue Jul  7 18:05:45 2020
 
 from flask import Flask, request, render_template
 from scrap_quote import quotes_list
-from flask_cors import CORS
+
 
 app=Flask(__name__)
-CORS(app)
 
 @app.route('/')
-
 def home():
     return render_template('home.html')
     
 @app.route('/search' , methods=['GET', 'POST'])
-
 def search():
-    input_topic=request.args.get("topic")
-    return render_template('home.html', quotes_result= quotes_list(input_topic))
+    input_topic=request.form['topic']
+    return render_template('result.html', quotes_result= quotes_list(input_topic,2))
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
+    
+    
     
     
